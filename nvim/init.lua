4eaeab9881fdb97e7faa1223af6eb1b0a3102691
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-03-24 11:31:23 -0300

    add install for mac

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
+require('packer_init')
 require('core/settings')
 require('core/keymaps')
 require('core/colors')
-require('packer_init')
 require('plugins/nvim-tree')
 require('plugins/nvim-treesitter')
 require('plugins/indent-blankline')
