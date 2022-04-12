fd6b9dd76beb7f13b0ea40af47397f6f797d1c86
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-04-11 22:21:01 -0300

    remove fzf integration

--- nvim/lua/packer_init.lua
+++ nvim/lua/packer_init.lua
@@ -32,12 +32,12 @@ return require('packer').startup(function(use)
 
   use 'famiu/feline.nvim'
 
+  use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
+
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   if packer_bootstrap then
     require('packer').sync()
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,4 @@
 require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    }
-  },
   defaults = {
     file_ignore_patterns = {
       "node_modules",
@@ -16,4 +8,4 @@ require('telescope').setup {
 }
 
 
-require('telescope').load_extension('fzf')
+-- require('telescope').load_extension('fzf')
