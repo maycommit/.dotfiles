bf22e13013470d1e63a778df3e119dc685e21a0d
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-05-26 08:56:50 -0300

    View hidden files

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,35 +1,3 @@
-local g = vim.g
-
-g.nvim_tree_width = 32
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_respect_buf_cwd = 1
-g.nvim_tree_root_folder_modifier = ':~'
-g.nvim_tree_symlink_arrow = ' >> '
-g.nvim_tree_width_allow_resize = 1
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 0,
-  folder_arrows = 1
-}
-g.nvim_tree_icons = {
-  default = " ",
-  git = {
-    unstaged = "✗",
-    staged = "✓",
-    unmerged = " ",
-    renamed = "➜",
-    untracked = "★"
-  },
-  folder = {
-    arrow_open = "- ",
-    arrow_closed = "> ",
-    default = "> ",
-    open = "- "
-  }
-}
-
 require('nvim-tree').setup {
   open_on_setup = false,
   update_cwd = true,
@@ -49,7 +17,7 @@ require('nvim-tree').setup {
     },
   },
   filters = {
-    dotfiles = false,
+    dotfiles = true,
     custom = { '.git', 'node_modules', '.cache', '.bin' },
   },
   renderer = {
